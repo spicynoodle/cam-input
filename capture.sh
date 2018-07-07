3881af3b1db043a5313d6eb450178582f52cd804
Randy Cahyana <ranren.cahyana@gmail.com>
2018-07-07 16:56:05 +0700

    remove coordinate from file name

--- capture.sh
+++ capture.sh
@@ -8,15 +8,15 @@ streamer -c /dev/video0 -b 256 -o $out
 
 ## add geotag
 coordinates=$(wget https://whataremycoordinates.com/ -O - -o /dev/null | pup '.coordinates' | sed -n '2p')
-lat=$(echo $coordinates | awk -F',' '{print$1}')
-lon=$(echo $coordinates | awk -F',' '{print$2}' | xargs)
+lat=$(echo $coordinates | awk -F',' '{print$1}' | sed 's/\./?/g')
+lon=$(echo $coordinates | awk -F',' '{print$2}' | sed 's/\./?/g' |xargs)
 echo $lat
 echo $lon
 
 exiftool -overwrite_original -GPSLongitude=$lat -GPSLatitude=$lon $out
 
 ## use it siganture as a file name
-filename=$(identify -verbose $out | grep signature | awk -F':' '{print$2}' | xargs)%$lat%$lon%.jpeg
+filename=$(identify -verbose $out | grep signature | awk -F':' '{print$2}' | xargs).jpeg
 echo $filename
 
 ## upload to s3
